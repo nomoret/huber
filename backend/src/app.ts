7fe207927acdbffd3bc15f3e97839451b445df9e
br j <honuj9983@gmail.com>
2021-01-01 19:52:59 +0900

    Add req in context, private middleware, get profile query

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,10 @@
+import { NextFunction, Response } from "express";
 import { GraphQLServer } from "graphql-yoga";
 import cors from "cors";
 // import helmet from "helmet";
 import logger from "morgan";
 import schema from "./schema";
+import decodeJWT from "./utils/decodeJWT";
 
 class App {
   public app: GraphQLServer;
@@ -10,6 +12,11 @@ class App {
   constructor() {
     this.app = new GraphQLServer({
       schema,
+      context: (req) => {
+        return {
+          req: req.request,
+        };
+      },
     });
     this.middlewares();
   }
@@ -18,6 +25,24 @@ class App {
     this.app.express.use(cors());
     this.app.express.use(logger("dev"));
     // this.app.express.use(helmet());
+    this.app.express.use(this.jwt);
+  };
+
+  private jwt = async (
+    req,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> => {
+    const token = req.get("X-JWT");
+    if (token) {
+      const user = await decodeJWT(token);
+      if (user) {
+        req.user = user;
+      } else {
+        req.user = undefined;
+      }
+    }
+    next();
   };
 }
 
